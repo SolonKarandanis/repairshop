187e7310c58e6b063f7e80eebd6d313b502a84e0
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-21 07:39:31 +0200

    Added TicketForm

--- src/app/(rs)/tickets/form/TicketForm.tsx
+++ src/app/(rs)/tickets/form/TicketForm.tsx
@@ -11,6 +11,10 @@ import {
     type selectTicketSchemaType 
 } from "@/zod-schemas/ticket"
 import { selectCustomerSchemaType } from "@/zod-schemas/customer"
+import { InputWithLabel } from "@/app/components/inputs/InputWithLabel"
+import { CheckboxWithLabel } from "@/app/components/inputs/CheckboxWithLabel"
+import { TextAreaWithLabel } from "@/app/components/inputs/TextAreaWithLabel"
+import { Button } from "@/components/ui/button"
 
 type Props = {
     customer: selectCustomerSchemaType,
@@ -56,7 +60,62 @@ export default function TicketForm({
                     className="flex flex-col sm:flex-row gap-4 sm:gap-8"
                 >
 
-                    <p>{JSON.stringify(form.getValues())}</p>
+                    <div className="flex flex-col gap-4 w-full max-w-xs">
+                        <InputWithLabel<insertTicketSchemaType>
+                            fieldTitle="Title"
+                            nameInSchema="title"
+                        />
+
+                        <InputWithLabel<insertTicketSchemaType>
+                            fieldTitle="Tech"
+                            nameInSchema="tech"
+                            disabled={true}
+                        />
+
+                        <CheckboxWithLabel<insertTicketSchemaType>
+                            fieldTitle="Completed"
+                            nameInSchema="completed"
+                            message="Yes"
+                        />
+
+                        <div className="mt-4 space-y-2">
+                            <h3 className="text-lg">Customer Info</h3>
+                            <hr className="w-4/5" />
+                            <p>{customer.firstName} {customer.lastName}</p>
+                            <p>{customer.address1}</p>
+                            {customer.address2 ? <p>{customer.address2}</p> : null}
+                            <p>{customer.city}, {customer.state} {customer.zip}</p>
+                            <hr className="w-4/5" />
+                            <p>{customer.email}</p>
+                            <p>Phone: {customer.phone}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 w-full max-w-xs">
+                        <TextAreaWithLabel<insertTicketSchemaType>
+                            fieldTitle="Description"
+                            nameInSchema="description"
+                            className="h-96"
+                        />
+                        <div className="flex gap-2">
+                            <Button
+                                type="submit"
+                                className="w-3/4"
+                                variant="default"
+                                title="Save"
+                            >
+                                Save
+                            </Button>
+
+                            <Button
+                                type="button"
+                                variant="destructive"
+                                title="Reset"
+                                onClick={() => form.reset(defaultValues)}
+                            >
+                                Reset
+                            </Button>
+                        </div>
+                    </div>
 
                 </form>
             </Form>
